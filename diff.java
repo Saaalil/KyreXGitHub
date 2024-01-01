6a004339bfa8a6cb0b4d4307f944fcc15b83986d
Saaalil/KyreX <salilhiremath2712@gmail.com>
2024-01-01 13:34:08 +0530

    adding Diff.java

---
+++ diff.java
@@ -0,0 +1,16 @@
+import java.util.Scanner; // Import the Scanner class
+
+class MyClass2 {
+  public static void main(String[] args) {
+    int x, y, sum;
+    Scanner myObj = new Scanner(System.in); // Create a Scanner object
+    System.out.println("Type a number:");
+    x = myObj.nextInt(); // Read user input
+
+    System.out.println("Type another number:");
+    y = myObj.nextInt(); // Read user input
+
+    sum = x - y;  // Calculate the sum of x + y
+    System.out.println("Diff is of two numbers given by u is : " + sum); // Print the sum
+  }
+} 
